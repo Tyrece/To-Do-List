733c6055a17926ef27c26444db8bac537b0a6d4e
Tyrece <tyreceb1@gmail.com>
2019-05-13 15:55:14 -0400

    add,delete tasks & add the login thing

--- client/main.js
+++ client/main.js
@@ -1,8 +1,60 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Accounts } from 'meteor/accounts-base';
 
 import './main.html';
 import '../lib/Collections.js';
+
+
+
+Accounts.ui.config({});
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY',
+});
+
+
+Template.AddTask.events({
+	'click .js-addTask'(){
+		console.log("hello")
+		$("#addTaskModal").modal("show")
+	},
+
+	'click .js-cancelTask'(){
+		$("#Task").val('');
+		$("#addTaskModal").modal("hide")
+		console.log("bye")
+	},
+
+	'click .js-saveTask'(){
+		var Task = $("#Task").val();
+		console.log("save", Task);
+
+		//$("#Task").val('');
+		$("#addTaskModal").modal("hide")
+		userDB.insert({"Task": Task,})
+	},
+})
+
+
+Template.Task.helpers({
+	allTask(){
+		return userDB.find({});
+	},
+
+});
+
+Template.Task.events({
+		'click .js-deleteTask'(){
+		var taskID = this._id
+		$("#"+taskID).fadeOut('slow',function(){
+			userDB.remove({_id:taskID});
+			console.log("delete", taskID)
+
+		})
+	},
+});
+
+
 
 
 
